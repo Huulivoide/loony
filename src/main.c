024eae39ca9bfd1088acfbfeaa08ea6f08b9861e
dreamyeyed <dreamyeyedprods@gmail.com>
2014-09-19 22:20:56 +0300

    added very basic functionality
    Currently you can append to a buffer, list the contents of that buffer
    and write the buffer in a while. There is no way to delete anything yet,
    but at least there's a good starting point.

--- src/main.c
+++ src/main.c
@@ -5,9 +5,54 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void save_buf (const char *filename, const char *buf)
+{
+    FILE *fp = fopen(filename, "w");
+    if (!fp) {
+        fprintf(stderr, "Couldn't open file %s for writing", filename);
+        return;
+    }
+    while (*buf) {
+        putc(*buf, fp);
+        buf++;
+    }
+    fclose(fp);
+}
 
 int main (int argc, char *argv[])
 {
-    printf("Hello, user! This is Loony. \n");
-    return 0;
+#define BUFSIZE 1024
+    char fbuf[BUFSIZE];
+    memset(fbuf, 0, BUFSIZE);
+    size_t used_fbuf = 1; /* NUL */
+    char buf[BUFSIZE];
+
+    if (argc != 2) {
+        printf("Loony must be launched with 'loony filename'\n");
+        return 1;
+    }
+
+    while (fgets(buf, BUFSIZE, stdin)) {
+        switch (buf[0]) {
+            case 'a':
+                strncat(fbuf, buf+1, BUFSIZE-used_fbuf);
+                used_fbuf += strlen(buf+1);
+                break;
+            case 'l':
+                printf("%s", fbuf);
+                break;
+            case 'w':
+                save_buf(argv[1], fbuf);
+                break;
+            case 'q':
+                exit(0);
+                break;
+            default:
+                fprintf(stderr, "Invalid command %c", *buf);
+                break;
+        }
+    }
 }

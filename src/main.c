bc5aec2ce88d4469d298c79d6d00c0cc3390da3f
dreamyeyed <dreamyeyedprods@gmail.com>
2014-09-19 21:03:02 +0300

    initial commit
    This commit contains some very basic stuff: .gitignore, autotools
    configs and a "skeleton" for main.c.

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ * This file contains the main() function and other stuff used at startup.
+ */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    printf("Hello, user! This is Loony. \n");
+    return 0;
+}

d9e99605a78ffa8e0bef6066d6f387d89c7e6e45
dreamyeyed <dreamyeyedprods@gmail.com>
2014-10-08 23:40:06 +0300

    added some utf-8 character functions

--- src/util.c
+++ src/util.c
@@ -10,7 +10,39 @@
 
 int is_u8_start_byte (char c)
 {
+    /* c is a start byte iff the first two bytes are *not* 10.
+     * iff == if and only if. That's not a typo. */
     return (c & 0xc0) != 0x80;
+}
+
+int is_u8_cont_byte(char c)
+{
+    /* c is a continuation byte iff the first two bits are 10. */
+    return (c & 0xc0) == 0x80;
+}
+
+int is_u8_ascii_char(char c)
+{
+    /* c is an ASCII character iff the first bit is zero. */
+    return !(c >> 7);
+}
+
+int u8_char_length(char c)
+{
+    if (!is_u8_start_byte(c)) {
+        return -1;
+    } else if (is_u8_ascii_char(c)) {
+        return 1;
+    } else {
+        /* Length in bytes == number of 1 bits before the first 0 bit starting
+         * from the left. */
+        int num_bytes = 1;
+        int mask;
+        for (mask = (1 << 6); c & mask; mask >>= 1) {
+            num_bytes++;
+        }
+        return num_bytes;
+    }
 }
 
 size_t u8strlen (const char *s)
--- src/util.h
+++ src/util.h
@@ -8,8 +8,19 @@
 
 #include <stddef.h>
 
-/* Returns 1 if c is a UTF-8 start byte; 0 if it isn't. */
+/* Returns 1 if c is a UTF-8 start byte (including an ASCII character);
+ * 0 if it isn't. */
 int is_u8_start_byte (char c);
+
+/* Returns 1 if c is a UTF-8 continuation byte; 0 if it isn't. */
+int is_u8_cont_byte(char c);
+
+/* Returns 1 if c is an ASCII character; 0 if it isn't. */
+int is_u8_ascii_char(char c);
+
+/* Returns the length of a UTF-8 character in bytes given its starting byte.
+ * Returns -1 if c is not a UTF-8 start byte. */
+int u8_char_length(char c);
 
 /* Calculates the number of codepoints in an UTF-8 encoded string. */
 size_t u8strlen (const char *s);

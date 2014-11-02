39707251414dcb5bb0e36b077cc1ece6ad1e503d
dreamyeyed <dreamyeyedprods@gmail.com>
2014-11-02 02:21:17 +0200

    commented cursesio.h with doxygen

--- src/cursesio.h
+++ src/cursesio.h
@@ -1,20 +1,33 @@
-#pragma once
-
-/*
- * cursesio.h
+/**
+ * @file cursesio.h
  *
- * This file is used for I/O using the curses library.
+ * Input / output using the curses library.
  */
+
+#pragma once
 
 #include <curses.h>
 
 #include "textbuf.h"
 
-/* Shows the contents of buf on the screen. */
+/**
+ * Shows the contents of the given buffer on the screen.
+ *
+ * @param buf the buffer to be displayed
+ */
 void display_buf(TextBuffer *buf);
 
-/* Allows the user to add a new line in the given position. */
+/**
+ * Allows the user to write a new line of text.
+ *
+ * @param buf the buffer where the new text should be added
+ * @param pos the line number of the new line
+ */
 void write_new_line(TextBuffer *buf, size_t pos);
 
-/* Inserts more text at the cursor. */
+/**
+ * Inserts more text at the cursor.
+ *
+ * @param buf the buffer where the next text should be added
+ */
 void insert_at_cursor(TextBuffer *buf);
